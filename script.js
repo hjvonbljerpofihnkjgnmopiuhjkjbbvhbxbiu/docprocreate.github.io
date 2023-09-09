01fde06e0c1954928c17647deefd48fc798a1874
hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu <58358771+hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu@users.noreply.github.com>
2023-09-09 16:31:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ $(function(){
             });
   });
 });
+
+var documentation = [
+  {
+    title: "Interface",
+    url: "interface.html",
+    content: "Ceci est la documentation pour l'interface utilisateur."
+  },
+  {
+    title: "Pinceaux",
+    url: "pinceaux.html",
+    content: "Ceci est la documentation pour les pinceaux."
+  }
+  // Ajoutez d'autres résultats prédéfinis ici
+];
+
+  $('form').submit(function (e) {
+    e.preventDefault(); // Empêche la soumission du formulaire
+
+    var searchTerm = $('.search').val().toLowerCase();
+
+    // Recherche dans la documentation
+    var result = documentation.find(function (doc) {
+      return doc.title.toLowerCase() === searchTerm;
+    });
+
+    if (result) {
+      // Redirection vers la page de documentation trouvée
+      window.location.href = result.url;
+    } else {
+      alert("Aucun résultat trouvé pour la recherche : " + searchTerm);
+    }
+  });
+});
